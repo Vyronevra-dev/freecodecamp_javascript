aefd8ace458cf033a039aeb9a616a536d97a8457
Vyronevra-dev <pabloevra27@gmail.com>
2026-06-10 15:38:15 +0300

    created a file

---
+++ functions/arrow_functions/example1.js
@@ -0,0 +1,15 @@
+const greetings = (name) => {
+	console.log(`Hello, ${name}!`);
+}
+
+greetings("Aaron");
+
+
+
+// =========================
+// DECLARATION IN ONE LINE
+// =========================
+
+const greeting = (name) => console.log("Hello, " + name + "!");
+
+greeting("Pablo");
---
+++ functions/arrow_functions/example2.js
@@ -0,0 +1,6 @@
+const calculateArea = (length, width) => {
+	const area = length * width;
+	return `The area of the rectangle is ${area} square units.`;
+};
+
+console.log(calculateArea(5, 10));
