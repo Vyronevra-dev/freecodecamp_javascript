d89d2411c9072e1e3ac52462eb613d4de46ea5f9
Vyronevra-dev <pabloevra27@gmail.com>
2026-07-10 07:20:02 +0300

    feat: added question marks to the questions

--- labs/quiz-game.js
+++ labs/quiz-game.js
@@ -2,35 +2,35 @@ const questions = [
 
   {
     category: "CHEMISTRY",
-    question: "What is the catalyst used to crack long chain alkanes into shorter ones",
+    question: "What is the catalyst used to crack long chain alkanes into shorter ones?",
     choices: ["Pt", "Al2O3", "CuO"],
     answer: "Al2O3"
   },
   
   {
     category: "ASTRONOMY",
-    question: "What is the closest star to Earth",
+    question: "What is the closest star to Earth?",
     choices: ["Mars", "Sun", "Jupiter"],
     answer: "Sun"
   },
   
   {
     category: "GEOGRAPHY",
-    question: "What is the capital city of France",
+    question: "What is the capital city of France?",
     choices: ["Bonjour", "Eiffel", "Paris"],
     answer: "Paris"
   },
   
   {
     category: "BIOLOGY",
-    question: "What pigment makes plant leaves look green",
+    question: "What pigment makes plant leaves look green?",
     choices: ["Chloroplasts", "Chlorophyll", "Stomats"],
     answer: "Chlorophyll"
   },
   
   {
     category: "Geometry",
-    question: "How many sides does a standard stop sign have",
+    question: "How many sides does a standard stop sign have?",
     choices: ["Five", "Four", "Eight"],
     answer: "Eight"
   },
