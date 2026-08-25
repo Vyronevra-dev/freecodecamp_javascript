5b068870e9857922246990613d17f2f51e1731b5
Vyronevra-dev <pabloevra27@gmail.com>
2026-08-25 13:00:57 +0300

    feat: created files on graphs and trees

---
+++ graphs-n-trees/graph.js
@@ -0,0 +1,34 @@
+/* A graph is a data structure used to represent relationships between objects.
+ * A graph is a data structure used to represent things (vertices/nodes) and 
+ * the relationships between them (edges).
+ * For example, a social network can be represented as:
+ * Alice ─── Bob
+     │        │
+     │        │
+  Charlie ─ David
+
+* The two main components of a graph are Nodes and edges
+* Nodes (vertices) → represent the objects.
+* Edges → represent the relationships/connections between those objects.
+*
+*   A
+   / \
+  B   C
+
+*Nodes: A, B, C
+
+Edges:
+A → B
+A → C
+
+* A directed graph is A graph where edges have a direction
+* In a directed graph, an edge indicates a specific direction. 
+* E.g A → B . This means there is a connection from A to B.
+*
+* An undirected graph A ─ B
+* Here the relationship goes both ways
+* A real-world example of a directed graph is Twitter/X follows:
+* Richard → Alice . 
+* Richard following Alice doesn't automatically mean Alice follows Richard.
+*
+ */ 
