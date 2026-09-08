4c1e5b61813725425fb8c2b037ad9a510161cdf5
Vyronevra-dev <pabloevra27@gmail.com>
2026-09-08 10:46:15 +0300

    feat: create string repeating function using js

--- labs/string-repeating-function.js
+++ labs/string-repeating-function.js
@@ -13,4 +13,12 @@ function repeatStringNumTimes(str, num) {
   return result;
 }
 
-console.log(repeatStringNumTimes("Messi!!! ", 5))
+console.log(repeatStringNumTimes("Messi!!! ", 5));
+console.log(repeatStringNumTimes("*", 3));
+console.log(repeatStringNumTimes("abc", 3));
+console.log(repeatStringNumTimes("abc", 4));
+console.log(repeatStringNumTimes("abc", 1));
+console.log(repeatStringNumTimes("*", 8));
+console.log(repeatStringNumTimes("abc", 3));
+console.log(repeatStringNumTimes("abc", -2));
+console.log(repeatStringNumTimes("abc", 0));
