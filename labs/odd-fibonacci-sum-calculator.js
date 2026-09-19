fa75a0aa4595c2e0834c7b3291b7c28191a4255c
Vyronevra-dev <pabloevra27@gmail.com>
2026-09-19 13:54:57 +0300

    feat: created an odd fibonacci sum calculator using js

---
+++ labs/odd-fibonacci-sum-calculator.js
@@ -0,0 +1,18 @@
+function sumFibs(num) {
+  let prev = 0;
+  let curr = 1;
+  let sum = 0;
+
+  while (curr <= num) {
+    if (curr % 2 !== 0) {
+      sum += curr;
+    }
+    
+    // Generate the next Fibonacci number
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return sum;
+}
